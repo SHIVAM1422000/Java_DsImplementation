ca22991bc5854e94f93cee97ba7bf1803d5e8cdf
SHIVAM1422000 <shivamojha1422000@gmail.com>
2021-09-28 07:05:50 +0530

    Added Ds Tries

---
+++ Tries/Tries.java
@@ -0,0 +1,22 @@
+public class Tries{
+ 
+
+
+Tries children[];    
+int isEnd;
+
+    
+public Tries(){
+   children=new Tries[26];
+   isEnd=0; 
+}
+
+
+}
+
+
+
+// truct TrieNode *children[ALPHABET_SIZE];
+//      // isEndOfWord is true if the node 
+//      // represents end of a word 
+//      bool isEndOfWord; 
